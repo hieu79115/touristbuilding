1b93591838a14f2ecf42bdc6b76aaeec3726ba71
hieu 79115 <npt911@gmail.com>
2023-12-19 14:41:17 +0700

    comple prim

--- src/context/rightsidebar/RightSidebar.tsx
+++ src/context/rightsidebar/RightSidebar.tsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from 'react';
 import { MapContext } from '../maps/MapContext';
 import './RightSidebar.css';
 import { Feature } from '../../interfaces/places';
+import { directionsApi } from '../../apis';
+import { DirectionsResponse } from '../../interfaces/directions';
 
 export const RightSidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +13,9 @@ export const RightSidebar = () => {
     const { listPlaces } = useContext(MapContext);
 
     const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
+    const [isContentVisible, setIsContentVisible] = useState(false);
+    const [primResult, setPrimResult] = useState<Edge[]>([]);
+    const [totalDistance, setTotalDistance] = useState<number>(0);
 
     const handleDragStart = (
         e: React.DragEvent<HTMLUListElement>,
@@ -57,7 +62,7 @@ export const RightSidebar = () => {
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
     };
-
+    
     useEffect(() => {
         console.log('List seleted Features:');
         console.log(selectedFeatures.length);
@@ -69,6 +74,154 @@ export const RightSidebar = () => {
         updateListPlaces(newListPlaces);
         console.log(`Deleting feature with ID: ${index}`);
     };
+
+    const handleFloatingButtonClick = async () => {
+        try {
+            const distances = await calculateDistances();
+            const graph = buildGraphFromDistances(distances);
+            const primResult = applyPrimAlgorithm(graph);
+            setPrimResult(primResult);
+            console.log('Prim Result:', primResult);
+
+            setIsContentVisible(true);  // Hiển thị lộ trình khi tính toán xong
+
+            // tinh tong chieu dai lo trinh
+            setTotalDistance(prevTotalDistance => {
+                const total = primResult.reduce((sum, edge) => sum + edge.weight, 0);
+                return total;
+            });
+        } catch (error) {
+            console.error('Error calculating distances:', error);
+        }
+    };
+    
+    const handleCloseButtonClick = () => {
+        setIsContentVisible(false);
+    };
+
+    interface RouteResult {
+        kms: number;
+        minutes: number;
+    }
+    
+    const getRouteBetweenPoints = async (
+        start: [number, number],
+        end: [number, number]
+    ): Promise<RouteResult | null> => {
+        try {
+            const resp = await directionsApi.get<DirectionsResponse>(
+                `/${start.join(',')};${end.join(',')}`
+            );
+    
+            const route = resp.data.routes[0];
+    
+            if (!route) {
+                console.error("No route found");
+                return null;
+            }
+    
+            const { distance, duration } = route;
+    
+            let kms = distance / 1000;
+            kms = Math.round(kms * 100) / 100; // Sửa lỗi chia 100
+    
+            const minutes = Math.floor(duration / 60);
+    
+            console.log("Route:", route);
+            console.log("Kilometers:", kms);
+            console.log("Minutes:", minutes);
+    
+            return { kms, minutes };
+        } catch (error) {
+            console.error("Error fetching route:", error);
+            return null;
+        }
+    };
+
+    const calculateDistances = async (
+        
+    ): Promise<RouteResult[][]> => {    
+        const distances: RouteResult[][] = []; 
+
+        for (let i = 0; i < selectedFeatures.length; i++) {
+            const element: RouteResult[] = []; 
+            for (let j = 0; j < selectedFeatures.length; j++) {
+                // Gọi hàm getRouteBetweenPoints với tọa độ của điểm bắt đầu và điểm kết thúc
+                const startCoordinates: [number, number] = [selectedFeatures[i].center[0], selectedFeatures[i].center[1]];
+                const endCoordinates: [number, number] = [selectedFeatures[j].center[0], selectedFeatures[j].center[1]];
+                // Gọi hàm và chờ cho kết quả (do hàm là bất đồng bộ)
+                try {
+                    // Gọi hàm và chờ cho kết quả (do hàm là bất đồng bộ)
+                    const routeResult = await getRouteBetweenPoints(startCoordinates, endCoordinates);
+    
+                    // Kiểm tra giá trị trả về của hàm getRouteBetweenPoints
+                    if (routeResult !== null) {
+                        // Nếu có giá trị trả về, thêm giá trị kms vào mảng element
+                        element.push(routeResult);
+                        console.log("Route calculation complete.");
+                        console.log(element);
+                    } else {
+                        // Xử lý trường hợp không tìm thấy tuyến đường
+                        console.error("No route found between", startCoordinates, "and", endCoordinates);
+                        // Thêm một giá trị mặc định vào mảng element, hoặc bạn có thể thêm bất kỳ giá trị nào khác tùy thuộc vào logic của ứng dụng
+                        element.push({ kms: 0, minutes: 0 });
+                    }
+                } catch (error) {
+                    console.error("Error calculating route:", error);
+                }
+
+            }
+            distances.push(element);
+        }
+        console.log(distances);
+        return distances;
+    };
+
+    interface Edge {
+        start: number;
+        end: number;
+        weight: number;
+    }
+
+    const buildGraphFromDistances = (distances: RouteResult[][]): Edge[] => {
+        const graph: Edge[] = [];
+    
+        for (let i = 0; i < distances.length; i++) {
+            for (let j = 0; j < distances[i].length; j++) {
+                const { kms, minutes } = distances[i][j];
+                graph.push({ start: i, end: j, weight: kms });
+            }
+        }
+    
+        return graph;
+    };
+    
+    const applyPrimAlgorithm = (graph: Edge[]): Edge[] => {
+        const visited: boolean[] = Array(graph.length).fill(false);
+        const result: Edge[] = [];
+        const priorityQueue: Edge[] = [];
+    
+        // Bắt đầu từ đỉnh 0 (có thể chọn bất kỳ đỉnh nào)
+        priorityQueue.push({ start: 0, end: 0, weight: 0 });
+    
+        while (priorityQueue.length > 0) {
+            priorityQueue.sort((a, b) => a.weight - b.weight);
+            const { start, end, weight } = priorityQueue.shift()!;
+    
+            if (!visited[end]) {
+                visited[end] = true;
+                result.push({ start, end, weight });
+    
+                // Thêm các đỉnh kề chưa được thăm vào hàng đợi ưu tiên
+                for (const edge of graph.filter(e => e.start === end && !visited[e.end])) {
+                    priorityQueue.push(edge);
+                }
+            }
+        }
+    
+        return result;
+    };
+
 
     return (
         <>
@@ -110,7 +263,41 @@ export const RightSidebar = () => {
                         </ul>
                     ))}
                 </div>
-                <button className="floating-button">Floating Button</button>
+
+                <button 
+                    onClick={handleFloatingButtonClick} 
+                    className="floating-button"
+                    >
+                    Floating Button
+                </button>
+                {isContentVisible && (
+                <div className="sidebar-content">
+                    <div className='header'>
+                        <p>Lộ trình</p>
+                        <button onClick={handleCloseButtonClick} className="close-button">
+                            <img
+                                width="10"
+                                height="10"
+                                src="https://img.icons8.com/ios/50/delete-sign--v1.png"
+                                alt="delete-sign--v1"
+                            />
+                        </button>
+                    </div>
+                    <div className='content'>
+                        <ul>
+                        {primResult.map((edge) => (
+                            edge.start !== edge.end && (
+                                <li key={`${edge.start}-${edge.end}`}>
+                                    {`${selectedFeatures[edge.start].text} -> ${selectedFeatures[edge.end].text} : ${edge.weight} kms`}
+                                </li>
+                            )
+                        ))}
+                        </ul>
+                        <p>Tổng Chiều Dài: {totalDistance.toFixed(2)} kms</p>
+                    </div>
+                </div>
+                
+            )}
             </div>
         </>
     );
